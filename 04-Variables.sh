11f36b6273ece1bfe33352ff2cce613776c86b99
Haritha <hari.devops2025@gmail.com>
2025-05-16 14:28:12 +0530

    hello world

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PERSON1=$1 #there should be no space  before or after equal 
-PERSON2=$2
+Modi=$1 #there should be no space  before or after equal 
+Trump=$2
 
-echo "$PERSON1:: Hey $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing."
-echo "$PERSON1:: I am fine too. What's up?"
-echo "$PERSON2:: Nothing, just going to Mars now, will you come?"
-echo "$PERSON1:: Sorry, you carry on! I will come once you come back."
+echo "$1:: Hey $2, How are you?"
+echo "$2:: Hello $1, I am fine. How are you doing."
+echo "$1:: I am fine too. What's up?"
+echo "$2:: Nothing, just going to Mars now, will you come?"
+echo "$1:: Sorry, you carry on! I will come once you come back."
